eeef8f338a277953d5410b9a9402ff976de5735f
satizz <sat.nep001@gmail.com>
2016-03-02 09:25:32 +0800

    changed the display and addd the search field

--- bought/admin.py
+++ bought/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
-from .models import item
+from bought.models import item
 
 # Register your models here.
+class itemAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields':['name']}),
+        ('Quantity',{'fields':['quan']}),
+        ('Rate',{'fields':['rate']}),
+        ('status',{'fields':['status']}),
+    ]
+    list_filter = ['date']
+    search_fields = ['name']
+    list_display = ('name','quan','rate','total')
 
-admin.site.register(item)
+
+admin.site.register(item, itemAdmin)
--- bought/models.py
+++ bought/models.py
@@ -5,10 +5,20 @@ from django.db import models
 # models are here.
 
 class item(models.Model):
+
+
     name = models.CharField(max_length = 60)
     quan = models.IntegerField('Quantity',default = 1)
     rate = models.IntegerField(default = 0)
     date = models.DateField('Bought In')
+    status = models.BooleanField('Sold', default = False)
+
+    def total(self):
+        return self.quan * self.rate
 
     def __str__(self):
         return self.name
+
+    def sold(self):
+        return self.status
+
